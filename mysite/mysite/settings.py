2de077f7bc3547731b975eb72c964c2377ab9dde
Karishma Bhatia <bkrish19@gmail.com>
2014-02-11 14:54:58 -0500

    added bootstarp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS= (os.path.join(BASE_DIR,"travels","templates"))
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"travels","static"),)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
