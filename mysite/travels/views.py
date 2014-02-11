93855e2e6eb043c31083dcb2fea917faea3d9e67
Karishma Bhatia <bkrish19@gmail.com>
2014-02-11 14:39:29 -0500

    added templates directory

--- mysite/travels/views.py
+++ mysite/travels/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django.template import Context, loader
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Rango says hello world!")
+	return render(request,"index.html")
