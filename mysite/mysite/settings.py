b2cfe2d7b2c75fc2852737cb9a4912395882d1b4
Karishma Bhatia <bkrish19@gmail.com>
2014-02-15 21:59:29 -0500

    adding setting for social auth

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,8 +30,15 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.twitter.TwitterBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
 # Application definition
+
+TWITTER_CONSUMER_KEY         = '9sJm5wZejpjEFTBoXf7AUg'
+TWITTER_CONSUMER_SECRET      = 'oCHlhIhh4cJPSvVeskByzR0peyLR6Q8eBbgrtXKewjE'
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -40,7 +47,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_auth',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'social_auth.context_processors.social_auth_by_type_backends',
+    'django.contrib.auth.context_processors.auth,
+)
+
+SOCIAL_AUTH_ENABLED_BACKENDS = ('twitter',)
+SOCIAL_AUTH_DEFAULT_USERNAME = 'new_social_auth_user'
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
